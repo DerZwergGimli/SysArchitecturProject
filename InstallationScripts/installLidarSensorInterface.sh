772a9eeed7b709446d89b532d8a044c5fab0ee73
yannick <paule22@online.de>
2019-06-23 21:50:26 +0200

    changed scripts

--- InstallationScripts/installLidarSensorInterface.sh
+++ InstallationScripts/installLidarSensorInterface.sh
@@ -1,2 +1,21 @@
-test
+#!/bin/bash
+
+download_lidar_sensor()
+{
+   git clone https://github.com/bmegli/xv11lidar-test.git;
+   sudo apt-get update
+   sudo apt-get install build-essential
+   ./xv11lidar-test make
+}
+
+while true; do
+    read -p "Do you wish to install lidar-sensor-interface? (y/n)" yn
+    case $yn in
+        [Yy]* ) echo "installing..."; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
 
